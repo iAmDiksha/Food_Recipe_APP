a6e6af275463c1ebc538eb7b04bbaa8ff6cf0e73
Diksha <jdiksha394@gmail.com>
2024-09-09 22:46:55 +0530

    responsiveness added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import Search from './Search'
 
 const Header = () => {
     return (
-        <div className="bg-black flex text-white">
-            <div className="flex flex-col justify-center items-center px-20 gap-5">
+        <div className="bg-black flex text-white h-[87vh] w-full">
+            <div className="flex flex-col justify-center items-center px-20 gap-5 text-center">
                 <h1 className="text-2xl md:text-4xl lg:text-5xl leading-[1.5] font-[myFont] uppercase" id="header-heading">Discover Your New Favorite Dish with Our <span className="text-[color:#7dd956]">Tasty Recipes!</span></h1>
-                <Search isCenter={false}/>
+                <Search isCenter={true}/>
             </div>
-            <img src={halfpizza} alt="" className="w-[300px] header-img" />
+            {/* <img src={halfpizza} alt="" className="w-[300px] header-img" /> */}
         </div>
     )
 }
